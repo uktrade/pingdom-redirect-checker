815901b8a114f04d1f4eb57aa3840478665eca92
Jayesh Patel <jayesh.patel@digital.trade.gov.uk>
2019-05-29 12:05:27 +0100

    Code cleanup

--- check_redirects/admin.py
+++ check_redirects/admin.py
@@ -5,7 +5,7 @@ from .models import Urllist, Responsetime
 
 @admin.register(Urllist)
 class url_list_admin(admin.ModelAdmin):
-	list_display = ('site_url', 'team', 'target_url', 'enable', 'broken_redirect', 'actual_target', 'slack_sent')
+	list_display = ('site_url', 'target_url', 'enable', 'broken_redirect', 'actual_target')
 
 @admin.register(Responsetime)
 class response_time_admin(admin.ModelAdmin):
